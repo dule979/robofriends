8caf707f06e76fc00661e832c892417ee0574c67
Dusan Ilic <dule979@gmail.com>
2022-04-15 05:25:38 +0200

    Updates

---
+++ precache-manifest.f4e31fa0948f6396bc5b4714fbd6eb48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "de0bbbc7e075bd036e3b",
+    "url": "/robofriends/static/css/main.d476c625.chunk.css"
+  },
+  {
+    "revision": "de0bbbc7e075bd036e3b",
+    "url": "/robofriends/static/js/main.de0bbbc7.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "f4b5ea253c05c06f6981",
+    "url": "/robofriends/static/css/2.5f7e2be3.chunk.css"
+  },
+  {
+    "revision": "f4b5ea253c05c06f6981",
+    "url": "/robofriends/static/js/2.f4b5ea25.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ce4f3b9c5dae2dc388ae3cc1ef0cd01b",
+    "url": "/robofriends/index.html"
+  }
+];
